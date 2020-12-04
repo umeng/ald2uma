1997f2616e8974195315b2740b303c287cccfdba
云猫🐱 <zhihao.pzh@alibaba-inc.com>
2020-12-04 12:03:52 +0800

    所有方法支持参数扁平化

--- hooks/extgame.js
+++ hooks/extgame.js
@@ -10,22 +10,12 @@ const ON_END = "end";
 const ON_INIT = "init";
 const ON_SET = "set";
 const STAGE_START = [PREFIX, STAGE, ON_START].join(JOINCHAR);
-const STAGE_END = [PREFIX, STAGE, ON_END].join(JOINCHAR);
 
-uma.revenue = function (data) {
-  uma.trackEvent([PREFIX, REVENUE, data.group].join(JOINCHAR), data);
-};
-var stagestarttime = 0;
-uma.stage = {
-  onStart: function (data) {
-    stagestarttime = Date.now();
-    uma.trackEvent(STAGE_START, data);
-  },
-  onEnd: function (data) {
-    data._um_sdu = stagestarttime !== 0 ? Date.now() - stagestarttime : 0;
-    uma.trackEvent(STAGE_END, data);
-  },
-  onRunning: function (data) {
+/**
+ * 参数扁平化
+ * @param {object} data 
+ */
+function _flatData(data){
     var ndt = {};
     for (var dt in data) {
       var tdt = data[dt];
@@ -37,15 +27,31 @@ uma.stage = {
         ndt[dt] = tdt;
       }
     }
-    uma.trackEvent([PREFIX, STAGE, ON_RUNNING, data.event].join(JOINCHAR), ndt);
+    return ndt;
+}
+uma.revenue = function (data) {
+  uma.trackEvent([PREFIX, REVENUE, data.group].join(JOINCHAR), _flatData(data));
+};
+var stagestarttime = 0;
+uma.stage = {
+  onStart: function (data) {
+    stagestarttime = Date.now();
+    uma.trackEvent(STAGE_START, _flatData(data));
+  },
+  onEnd: function (data) {
+    data._um_sdu = (stagestarttime !== 0 ? Date.now() - stagestarttime : 0);
+    uma.trackEvent([PREFIX, STAGE, ON_END, data.event].join(JOINCHAR), _flatData(data));
+  },
+  onRunning: function (data) {
+    uma.trackEvent([PREFIX, STAGE, ON_RUNNING, data.event].join(JOINCHAR), _flatData(data));
   },
 };
 uma.level = {
     onInitLevel: function(data){
-        uma.trackEvent([PREFIX,LEVEL,ON_INIT].join(JOINCHAR),data);
+        uma.trackEvent([PREFIX,LEVEL,ON_INIT].join(JOINCHAR),_flatData(data));
     },
     onSetLevel: function(data){
-        uma.trackEvent([PREFIX,LEVEL,ON_SET].join(JOINCHAR),data);
+        uma.trackEvent([PREFIX,LEVEL,ON_SET].join(JOINCHAR),_flatData(data));
     }
 }
 export default uma;
