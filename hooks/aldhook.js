4a1c5c261d541dc629c2680c4985521863d16786
云猫🐱 <zhihao.pzh@alibaba-inc.com>
2020-12-04 12:01:52 +0800

    重构阿拉丁适配器

--- hooks/aldhook.js
+++ hooks/aldhook.js
@@ -1,70 +1,27 @@
 import { aop } from "./util";
 /**
- * 自动适配阿拉丁
- * @param {object} context 当前小程序顶级对象
+ * 自动适配阿拉丁小游戏
+ * @param {object} context 当前小程序顶级对象比如wx、qq、tt、swan、my
  * @param {object} uma 友盟sdk的uma对象
  * @returns {arrary}  [context, uma]
  */
-const aldOnShareAppMessage = "aldOnShareAppMessage";
-const aldShareAppMessage = "aldShareAppMessage";
-const aldSendEvent = "aldSendEvent";
-const aldRevenue = "aldRevenue";
 export function hookAld(context, uma) {
-  let topfns = [
-    aldOnShareAppMessage,
-    aldShareAppMessage,
-    aldSendEvent,
-    aldRevenue,
-  ];
-  for (let i = 0; i < topfns.length; i++) {
-    let topfn = topfns[i];
-    context[topfn] = aop(context[topfn] || function () {}, function (...args) {
-      switch (topfn) {
-        case aldSendEvent:
-          uma.trackEvent(...args);
-          break;
-        case aldRevenue:
-          uma.revenue(...args);
-          break;
-        case aldOnShareAppMessage:
-          uma.onShareAppMessage(...args);
-          break;
-        case aldShareAppMessage:
-          uma.shareAppMessage(...args);
-          break;
-      }
-    });
+  function _wrapSubFns(source, target, sourceMethods, targetMethods) {
+    let oldSource = source;
+    for (let i = 0; i < sourceMethods.length; i++) {
+      let sourceMethod = sourceMethods[i];
+      let targetMethod = targetMethods ? targetMethods[i] : false;
+      source[sourceMethod] = aop(oldSource ? oldSource[sourceMethod] : function () {}, function (...args) {
+        if (targetMethod) {
+          target[targetMethod](...args);
+        } else {
+          target[sourceMethod](...args);
+        }
+      })
+    }
   }
-
-  var oldStage = context.aldStage;
-  context.aldStage = {
-    onStart: aop(oldStage ? oldStage.onStart : function () {}, function (
-      ...args
-    ) {
-      uma.stage.onStart(...args);
-    }),
-    onRunning: aop(oldStage ? oldStage.onRunning : function () {}, function (
-      ...args
-    ) {
-      uma.stage.onRunning(...args);
-    }),
-    onEnd: aop(oldStage ? oldStage.onEnd : function () {}, function (...args) {
-      uma.stage.onStart(...args);
-    }),
-  };
-  var oldLevel = context.aldLevel;
-  context.aldLevel = {
-    onInitLevel: aop(
-      oldLevel ? oldStage.onInitLevel : function () {},
-      function (...args) {
-        uma.level.onInitLevel(...args);
-      }
-    ),
-    onSetLevel: aop(oldLevel ? oldStage.onSetLevel : function () {}, function (
-      ...args
-    ) {
-      uma.level.onSetLevel(...args);
-    }),
-  };
+  _wrapSubFns(wx,uma,['aldSendEvent','aldRevenue','aldOnShareAppMessage','aldShareAppMessage'],['trackEvent','revenue','onShareAppMessage','shareAppMessage'])
+  _wrapSubFns(context.aldStage,uma.stage,['onStart','onRunning','onEnd']);
+  _wrapSubFns(context.aldLevel,uma.level,['onInitLevel','onSetLevel']);
   return [context, uma];
 }
